cf8d9d82a3791877d044dbb59bb9b9b96bb47f18
Callum Powell <callumpowell@outlook.com>
2014-08-25 00:36:20 +0200

    added rails project

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-KnockoutTest::Application.config.session_store :cookie_store, key: '_knockoutTest_session'
+Rails.application.config.session_store :cookie_store, key: '_promillekalkulator_session'
