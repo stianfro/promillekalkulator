8d267eb4dbe2243bb99d7cec31eef396fd93a82b
Callum Powell <callumpowell@outlook.com>
2016-06-23 23:11:14 +0200

    got rid of rails and kept the essentials

--- js/funcs.js
+++ js/funcs.js
@@ -1,11 +1,11 @@
 
 $(document).ready(function() {
-	$('.datepicker').pickadate({
+	/*$('.datepicker').pickadate({
 	    min: new Date(2013,3,20),
 	    max: new Date(2013,7,14)
 	})
 
-	$('.timepicker').pickatime()
+	$('.timepicker').pickatime()*/
 
 	$('#info-button').click(function() {
 		$('.overlay.sidebar')
